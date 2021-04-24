d06d2066e32916f377a57ba93aef6f67d4cff7a3
s0i37 <s0i37@work>
2021-04-24 13:57:29 +0500

    on_client/interfaces built-in implemented

---
+++ on_client/interfaces.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+from nmb.NetBIOS import NetBIOS # pip2 install pysmb
+from sys import argv
+
+print '[*] getting network interfaces via NetBIOS'
+
+ip = argv[1]
+netbios = NetBIOS()
+
+def interfaces(ip):
+	try:
+		netbios_names = netbios.queryIPForName( str(ip), timeout=0.1 )
+		if netbios_names:
+			print ', '.join( (netbios.queryName( netbios_names[0], ip=str(ip) ) or []) + netbios_names )
+	except Exception as e:
+		print str(e)
+
+interfaces(ip)
