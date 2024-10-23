dc673da9b0b5e1986d6b724437be390695f015f5
s0i37 <s0i37@work>
2024-10-23 17:39:33 +0500

    fixed error in on_network/sslsplit.sh

--- on_network/sslsplit.sh
+++ on_network/sslsplit.sh
@@ -14,7 +14,8 @@ if [ -z $(pgrep socat) ]; then
 	fi
 
 	#screen -dmS sslsplit sslsplit -k /tmp/key.pem -c /tmp/cert.pem -l /tmp/con.log -L sslsplit.log -P autossl 0.0.0.0 1080
-	#screen -dmS sslsplit 
+	#screen -dmS sslsplit
+	touch sslsplit.log
 	socat -v openssl-listen:1080,fork,cert=/tmp/cert_key.pem,cafile=/tmp/cert.pem,verify=0 open:sslsplit.log,creat,append 2> /dev/null &
 fi
 
