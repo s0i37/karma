2fe9d562c6e516afa222a252ac3e7d66ece7f1c5
s0i37 <s0i37@work>
2024-10-23 19:54:32 +0500

    cosmetic in on_network/http.sh

--- on_network/http.sh
+++ on_network/http.sh
@@ -2,9 +2,9 @@
 
 echo '[*] HTTP cleartext monitoring'
 
-ngrep -d "$1" -i 'cookie|passw|token' 'port 80 or port 8080' 2>&1 > /tmp/ngrep.log &
+ngrep -d "$1" -i 'cookie|passw|token' 'port 80 or port 8080' 2>&1 > http.log &
 
-tail -n 0 -f /tmp/ngrep.log | while read line
+tail -n 0 -f http.log | while read line
 do
 	echo "$line" | grep -v '^match:' | grep -ai -e cookie -e passw -e token --color=auto && led yellow on 2> /dev/null
 done
